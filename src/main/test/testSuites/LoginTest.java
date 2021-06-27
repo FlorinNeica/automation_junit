cb2009976072aaea701eea4a396f60630228ad2a
DESKTOP-1A2CE4N\ionel <ionelneica@yahoo.com>
2021-06-27 12:59:07 +0300

    Login Test

---
+++ src/main/test/testSuites/LoginTest.java
@@ -0,0 +1,54 @@
+package testSuites;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+	private WebDriver driver;
+
+	@Before
+	public void beforeTest() {
+
+		System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+
+		driver.get("http://siit.epizy.com/index.php?route=account/login");
+	}
+
+	@Test
+	public void loginInvalidCredentialsTest() {
+
+		WebElement txtEmailAddress = driver.findElement(By.id("input-email"));
+
+		txtEmailAddress.sendKeys("acasa1@home.com");
+
+		WebElement txtPassword = driver.findElement(By.id("input-password"));
+
+		txtPassword.sendKeys("acasa1");
+
+		WebElement btnLogin = driver.findElement(By.xpath("//input[@value='Login']"));
+
+		btnLogin.click();
+
+		WebElement lblValidationError = driver.findElement(By.xpath("//div[@class='alert alert-danger alert-dismissible']"));
+
+		String actualErrorMsg = lblValidationError.getText();
+
+		String expectedErrorMsg = "Warning: No match for E-Mail Address and/or Password.";
+
+		Assert.assertEquals("The validation error is incorrect", expectedErrorMsg, actualErrorMsg);
+	}
+
+	@After
+	public void afterTest() {
+		driver.quit();
+	}
+}
